f7700f1aa7ac89a0fe3d36a9186c76e4eb694d45
jose <jbermejop01@elpuig.xeill.net>
2020-10-07 18:45:10 +0200

    Initial commit

--- src/Uf1/Ejercicio1.java
+++ src/Uf1/Ejercicio1.java
@@ -7,18 +7,29 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Escribe la direccion:");
-        String path = sc.nextLine();
-        File f = new File(path);
-        String[] archivos = f.list();
-        for (int i = 0; i < archivos.length; i++) {
-            System.out.println(archivos[i]);
+        String ruta = sc.nextLine();
+        File f = new File(ruta);
+        boolean path_Existe=false;
+
+        while (path_Existe !=true)
+        if (f.exists()== true){
+            path_Existe=true;
+            String[] archivos = f.list();
+            for (int i = 0; i < archivos.length; i++) {
+                System.out.println(archivos[i]);
+            }
+            System.out.println(" \n --------------------------------- \n");
+
+            File[] roots = File.listRoots();
+            for (File root : roots) {
+                System.out.println(root);
+            }
+        }else{
+            System.out.println("path invalido pon uno nuevo:");
+            ruta=sc.nextLine();
+            f = new File(ruta);
         }
 
-        System.out.println(" \n --------------------------------- \n");
 
-        File[] roots = File.listRoots();
-        for (File root : roots) {
-            System.out.println(root);
-        }
     }
 }
